d5c9879a5b300382950f447e9a9adbc8b607b964
unknown <laoda1144@gmail.com>
2021-05-28 18:13:51 +0800

    3rd commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import json
 from spirecomm.communication.coordinator import Coordinator
 from spiremine.Crypto.blocks import Block
 from spiremine.Crypto.blocks import Blockchain
@@ -6,6 +6,7 @@ from spiremine.Crypto.blocks import Blockchain
 if __name__ == '__main__':
     coordinator = Coordinator()
     coordinator.signal_ready()
-    my_file = open('test.txt', 'w')
-    my_file.write('Communication Secured')
-    
+    with open('text.txt', 'w') as f:
+        f.write('readme')
+        f.close()
+        
